ab209c393f2626be5269e151ec2ad7802ab9073a
Naugtur <naugtur@gmail.com>
2019-08-02 22:37:41 +0200

    allow setting mock path

--- pkgFacade/mock.js
+++ pkgFacade/mock.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
+const argv = require('../arguments').get()
 
 module.exports = {
     getAudit() {
         console.log('>>>mock get audit')
-        return Promise.resolve(fs.readFileSync(`mock-audit.json`))
+        if (typeof argv.mock === 'string') {
+            return Promise.resolve(fs.readFileSync(argv.mock))
+        } else {
+            return Promise.resolve(fs.readFileSync('mock-audit.json'))
+        }
     }
 }
