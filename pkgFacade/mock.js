dd344593bfc48d04ab559a426d4e18a5b4bf077d
Naugtur <naugtur@gmail.com>
2019-09-19 11:37:46 +0200

    mock to also return parsed json

--- pkgFacade/mock.js
+++ pkgFacade/mock.js
@@ -5,9 +5,9 @@ module.exports = {
     getAudit() {
         console.log('>>>mock get audit')
         if (typeof argv.mock === 'string') {
-            return Promise.resolve(fs.readFileSync(argv.mock))
+            return Promise.resolve(JSON.parse(fs.readFileSync(argv.mock)))
         } else {
-            return Promise.resolve(fs.readFileSync('mock-audit.json'))
+            return Promise.resolve(JSON.parse(fs.readFileSync('mock-audit.json')))
         }
     }
 }
